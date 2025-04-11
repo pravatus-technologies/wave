ca927c83ed03974bc56d09a0f8b27a92d6067b46
Ephraim General <ephraim.general@pravatustech.com>
2025-04-11 17:09:49 -0500

    added signup screen

---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,223 @@
+import {
+  ActionButton,
+  FormInput,
+  FormToggle,
+  Icon,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "@/components";
+import { useAuth, useData, useTheme } from "@/hooks";
+import { useNavigation, useRouter } from "expo-router";
+import { useCallback, useEffect, useLayoutEffect, useState } from "react";
+import { Pressable, Image, Platform, StyleSheet } from "react-native";
+import { isValidDateFormat, isOfLegalAge } from "@/utils";
+import * as regex from "@/constants/regex";
+
+interface IRegisterForm {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  agreeToTerms: boolean;
+}
+
+interface IRegisterValidation {
+  email: boolean;
+  password: boolean;
+  firstName: boolean;
+  lastName: boolean;
+  birthday: boolean;
+  isLegalAge: boolean;
+  agreeToTerms: boolean;
+}
+
+export default function Signup() {
+  const navigation = useNavigation();
+  const { colors, sizes, assets } = useTheme();
+  const { registerUserWithEmail } = useAuth();
+  const { isDark } = useData();
+  const router = useRouter();
+
+  const [busy, setBusy] = useState(false);
+  const [register, setRegisterData] = useState<IRegisterForm>({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    birthday: "",
+    agreeToTerms: false,
+  });
+
+  const [isValid, setIsValid] = useState<IRegisterValidation>({
+    email: false,
+    password: false,
+    firstName: false,
+    lastName: false,
+    birthday: false,
+    isLegalAge: false,
+    agreeToTerms: false,
+  });
+
+  const handleChange = useCallback((value: Partial<IRegisterForm>) => {
+    setRegisterData((prev) => ({ ...prev, ...value }));
+  }, []);
+
+  const handleSignup = useCallback(async () => {
+    try {
+      setBusy(true);
+      await registerUserWithEmail(
+        register.email,
+        register.password,
+        register.firstName
+      );
+      router.replace("/");
+    } catch (error: any) {
+      let errorMessage = "Something went wrong. Please try again";
+    }
+    setBusy(false);
+  }, [register]);
+
+  useEffect(() => {
+    setIsValid({
+      email: regex.email.test(register.email),
+      password: regex.password.test(register.password),
+      firstName: regex.name.test(register.firstName),
+      lastName: regex.name.test(register.lastName),
+      birthday:
+        isValidDateFormat(register.birthday) && isOfLegalAge(register.birthday),
+      isLegalAge: isOfLegalAge(register.birthday),
+      agreeToTerms: register.agreeToTerms,
+    });
+    console.log(`Validity: ${JSON.stringify(isValid)}`);
+  }, [register]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTransparent: true,
+      headerTitle: "",
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.goBack()}
+          style={{ paddingHorizontal: 5 }}
+        >
+          <Icon name="ArrowLeft" size={24} color={colors.primary} />
+        </Pressable>
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <SafeAreaView style={{ flex: 1, paddingHorizontal: 20 }}>
+      <View
+        style={{
+          margin: "auto",
+          alignContent: "center",
+          alignItems: "center",
+          marginTop: sizes.height * 0.15,
+        }}
+      >
+        <Image source={assets.logo} style={{ width: 64, height: 64 }} />
+      </View>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        keyboardVerticalOffset={60}
+      >
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={styles.scrollContent}
+          keyboardShouldPersistTaps="handled"
+        >
+          <FormInput
+            autoCapitalize="none"
+            placeholder="Email"
+            icon="MailIcon"
+            value={register.email}
+            onChangeText={(text) => handleChange({ email: text })}
+          />
+          <FormInput
+            secureTextEntry
+            autoCapitalize="none"
+            placeholder="Password"
+            icon="Lock"
+            value={register.password}
+            onChangeText={(text) => handleChange({ password: text })}
+          />
+          <FormInput
+            autoCapitalize="words"
+            placeholder="First Name"
+            value={register.firstName}
+            onChangeText={(text) => handleChange({ firstName: text })}
+          />
+          <FormInput
+            autoCapitalize="words"
+            placeholder="Last Name"
+            value={register.lastName}
+            onChangeText={(text) => handleChange({ lastName: text })}
+          />
+          <FormInput
+            autoCapitalize="none"
+            placeholder="Birthday (YYYY/MM/DD)"
+            value={register.birthday}
+            onChangeText={(text) => handleChange({ birthday: text })}
+          />
+          <FormToggle
+            label="Agree to terms"
+            value={register.agreeToTerms}
+            onValueChange={(v) => handleChange({ agreeToTerms: v })}
+          />
+          <ActionButton
+            title="Sign Up"
+            onPress={handleSignup}
+            loading={busy}
+            disabled={
+              !isValid.email ||
+              !isValid.password ||
+              !isValid.firstName ||
+              !isValid.lastName ||
+              !isValid.isLegalAge ||
+              !isValid.agreeToTerms
+            }
+            buttonStyle={{ backgroundColor: colors.primary, marginTop: 16 }}
+            spinnerColor={colors.white}
+          />
+        </ScrollView>
+        <View style={styles.footer}>
+          <ActionButton
+            title="Create an account"
+            onPress={() => router.push("/signup")}
+            textColor={colors.primary}
+            buttonStyle={{
+              backgroundColor: "transparent",
+              borderWidth: 1,
+              borderColor: isDark ? colors.gray : colors.primary,
+            }}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: "center",
+    paddingVertical: 24,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    backgroundColor: "transparent",
+    padding: 12,
+    marginVertical: 8,
+    borderRadius: 10,
+  },
+  footer: {
+    marginTop: "auto",
+    marginBottom: 20,
+  },
+});
