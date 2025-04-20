6c06090c0bdccf8b3c116caf35531efb28d0cf6e
Ephraim General <ephraim.general@pravatustech.com>
2025-04-20 16:30:53 -0500

    added step 3

---
+++ mobile/app/(auth)/signup/step3.tsx
@@ -0,0 +1,61 @@
+import { View, Text } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { FormButton, ImageButton, LinkButton } from '@components/controls';
+import { useTheme, useTranslation } from '@context';
+import { AppIcon } from 'src/components';
+
+export default function SignupStep3(): JSX.Element {
+  const { colors, sizes } = useTheme();
+  const { t } = useTranslation();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+      {/* header back button container */}
+      <View
+        style={{
+          alignItems: 'flex-start',
+          marginHorizontal: -3,
+        }}
+      >
+        {/* This is hidden */}
+        <ImageButton onPress={() => console.log()}>
+          <AppIcon name="ChevronLeft" size={sizes.m} color={colors.background} />
+        </ImageButton>
+      </View>
+      {/* header title container */}
+      <View style={{ padding: sizes.padding }}>
+        <Text style={{ fontFamily: 'OpenSans-Regular', fontSize: sizes.h1 }}>
+          {t('Time for a selfie!')}
+        </Text>
+        <Text style={{ fontFamily: 'OpenSans-Regular', fontSize: sizes.h5, marginTop: sizes.s }}>
+          {t("Let's show the world your smile")}
+        </Text>
+      </View>
+      {/* form buttons */}
+      <View style={{ padding: sizes.padding, marginTop: sizes.m }}>
+        <FormButton
+          title={t('Take a picture')}
+          onPress={() => console.log('Take a picture')}
+        ></FormButton>
+        <FormButton
+          containerStyle={{ marginTop: sizes.s }}
+          outline={true}
+          title={t('Create an account')}
+          onPress={() => console.log('Upload picture')}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            marginTop: sizes.m,
+          }}
+        >
+          <LinkButton disabled={false} onPress={() => console.log('Skip to onboarding')}>
+            <Text style={{ fontFamily: 'OpenSans-SemiBold' }}>{t("I'll do this later")}</Text>
+          </LinkButton>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
--- mobile/navigation/signup.tsx
+++ mobile/navigation/signup.tsx
@@ -5,6 +5,7 @@ export default function SignupLayout(): JSX.Element {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="step2" />
+      <Stack.Screen name="step3" />
     </Stack>
   );
 }
