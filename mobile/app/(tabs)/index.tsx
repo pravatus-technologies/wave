706ed6db7ae39ebdd0afe70ca9ea6124230ff06a
Ephraim General <ephraim.general@pravatustech.com>
2025-04-21 15:09:23 -0500

    linted index.tsx

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -2,13 +2,15 @@ import { Pressable, Text } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { useAuth } from '@context';
+import AppText from '@components/controls/AppText';
+import { useAuth, useTranslation } from '@context';
 
 export default function Home(): JSX.Element {
   const { logout } = useAuth();
+  const { t } = useTranslation();
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello World</Text>
+      <AppText>{t('hello')}</AppText>
       <Pressable onPress={logout}>
         <Text>Logout</Text>
       </Pressable>
